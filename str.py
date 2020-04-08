606f1ba3157efad15cb3f24468fcb80ed65e6a74
arabindo <arobinda7@gmail.com>
2020-04-08 20:25:56 +0530

    Changed the range of x and y in plot as well as in data. Changed the unit of velocity from m/s to unit of c. Changed the range of slider.

--- str.py
+++ str.py
@@ -6,11 +6,8 @@ from bokeh.models import ColumnDataSource, Slider, TextInput
 from bokeh.plotting import figure, output_file, show
 
 #set up data
-c= 30000000
-v= 200000
-m = v/c
-xd = 20000
-x = np.linspace(0, xd, xd)
+m = 0.2
+x = np.linspace([i for i in range(-150,150,1)])
 xn = m*x
 yn = x/m
 
@@ -22,13 +19,14 @@ TOOLS = "crosshair,pan,wheel_zoom,box_zoom,reset,box_select,lasso_select"
 
 p = figure(plot_height=400, plot_width=400,
             tools=  TOOLS, title="Minkowski Doagram",
+            x_range=[-150,150], y_range=[-150,150],
             x_axis_label="x", y_axis_label="ct")
 p.line(x, xn, legend_label="x'", line_color="red", line_dash="4 4")
 p.line(x, yn, legend_label="ct'",line_color="blue", line_dash="4 4")
 
 
 #set up widgets
-velocity = Slider(title="velocity", value=200000, start=-29999999, end=29999999, step=500)
+velocity = Slider(title="velocity(c)", value=0.2, start=-0.99, end=0.99, step=0.01)
 
 
 #set up call back
