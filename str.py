3724ad3de752142dac8080c55de3fc3bf461e35c
arabindo <arobinda7@gmail.com>
2020-04-08 15:12:25 +0530

    Add a new file str.py

---
+++ str.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+from bokeh.io import curdoc
+from bokeh.layouts import column, row
+from bokeh.models import ColumnDataSource, Slider, TextInput
+from bokeh.plotting import figure
+
+#set up data
+
+
+#set up plots
+
+
+#set up widgets
+
+
+#set up call back
+
+#set up layout
+
